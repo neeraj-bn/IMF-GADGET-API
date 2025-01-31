6e45b29a67f6bb91e7814df3b1c380b64b7e0d0b
neeraj <neerajb2662001@gmail.com>
2025-01-31 18:14:39 +0530

    Update database connection to use environment variables and enable dialect module for PostgreSQL

--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config(); // Load environment variables
 
+
+console.log("sssssssssss",process.env.DB_HOST);
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "postgres", // Use "mysql" if MySQL is used
+  dialectModule: "pg",
+
   logging: false, // Set to true for debugging
 });
 
--- index.js
+++ index.js
@@ -9,20 +9,20 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: "postgres" });
+const sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: "postgres",dialectModule: "pg"});
 
 // Import Routes
 app.get("/", (req, res) => {
   res.json({ message: "Server running" });
 });
-// const gadgetRoutes = require("./routes/gadgetRoutes");
-// app.use("/gadgets", gadgetRoutes);
+const gadgetRoutes = require("./routes/gadgetRoutes");
+app.use("/gadgets", gadgetRoutes);
 
-// const authRoutes = require("./routes/authRoutes");
-// app.use("/user", authRoutes);
+const authRoutes = require("./routes/authRoutes");
+app.use("/user", authRoutes);
 
 app.listen(3000, async () => {
-  // await sequelize.authenticate();
+  await sequelize.authenticate();
   console.log("Server running on port 3000");
 });
 
